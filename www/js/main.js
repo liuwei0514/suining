63ab4450554ccd3d0b2f61d970d22fa705296d30
liuwei0514 <google.com>
2014-03-31 10:59:03 +0800

    加上 joap

--- www/js/main.js
+++ www/js/main.js
@@ -1,82 +1,30 @@
-window.HomeView = Backbone.View.extend({
+    require.config({　　　　
+        paths: {　　　　　　
+            "jquery": "lib/jquery-2.1.0.min",
+            "underscore": "lib/underscore-min",
+            "backbone": "lib/backbone-min",
+            "jquerymobile": "lib/jquery.mobile-1.4.2.min" ,
+            "jquerysoap": "lib/jquery.soap"　　　　
+        },
+        shim: {　　　　　　
+            'underscore': {　　　　　　　　
+                exports: '_'　　　　　　
+            },
+            "jquerysoap"  : ["jquery"],
+            "backbone": {
+                "deps": ["underscore", "jquery"],
+                "exports": "Backbone" //attaches "Backbone" to the window object
+            }　　　　
+        }　　
+    });
+    require(["jquery", "backbone", "mobileRouter", "jquerymobile"], function($, Backbone, Mobile) {
 
-    template:_.template($('#home').html()),
+        // Prevents all anchor click handling
+        $.mobile.linkBindingEnabled = false;
 
-    render:function (eventName) {
-        $(this.el).html(this.template());
-        return this;
-    }
-});
+        // Disabling this will prevent jQuery Mobile from handling hash changes
+        $.mobile.hashListeningEnabled = false;
 
-window.Page1View = Backbone.View.extend({
-
-    template:_.template($('#page1').html()),
-
-    render:function (eventName) {
-        $(this.el).html(this.template());
-        return this;
-    }
-});
-
-window.Page2View = Backbone.View.extend({
-
-    template:_.template($('#page2').html()),
-
-    render:function (eventName) {
-        $(this.el).html(this.template());
-        return this;
-    }
-});
-
-var AppRouter = Backbone.Router.extend({
-
-    routes:{
-        "":"home",
-        "page1":"page1",
-        "page2":"page2"
-    },
-
-    initialize:function () {
-        // Handle back button throughout the application
-        $('.back').live('click', function(event) {
-            window.history.back();
-            return false;
-        });
-        this.firstPage = true;
-    },
-
-    home:function () {
-        console.log('#home');
-        this.changePage(new HomeView());
-    },
-
-    page1:function () {
-        console.log('#page1');
-        this.changePage(new Page1View());
-    },
-
-    page2:function () {
-        console.log('#page2');
-        this.changePage(new Page2View());
-    },
-
-    changePage:function (page) {
-        $(page.el).attr('data-role', 'page');
-        page.render();
-        $('body').append($(page.el));
-        var transition = $.mobile.defaultPageTransition;
-        // We don't want to slide the first page
-        if (this.firstPage) {
-            transition = 'none';
-            this.firstPage = false;
-        }
-        $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
-    }
-
-});
-
-$(document).ready(function () {
-    console.log('document ready');
-    app = new AppRouter();
-    Backbone.history.start();
-});
+        // Instantiates a new Backbone.js Mobile Router
+        this.router = new Mobile();
+    });
